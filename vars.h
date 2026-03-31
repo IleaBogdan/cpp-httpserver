6600d9c14d06afe26ba64a5b77d8a0db5f4905a0
IleaBogdan <ilea.bogdan06@gmail.com>
2026-03-31 23:22:33 +0300

    note

--- vars.h
+++ vars.h
@@ -5,6 +5,11 @@
 #include"mail++/mail++.h"
 #include"env.h"
 
+// note to self and to Luca:
+// global vars declared in headers must be const or inline
+// global vars references must have the exter keyword (I belive, I am not sure, will check)
+// but the extern ones must be declared in a .cpp file
+
 const std::string DATABASE_PATH="test.db";
 inline sqlite3* db;
 inline sqlite3_stmt* stmt;
